225e8eaea321c666edfde7e9719dc599ed29f376
Antonio Jackson <antoniojackson@gmail.com>
2020-06-27 16:27:13 -0600

    Updating NearEarthObjects class to create asteroid objects at initialization and removing the asteroid_list, biggest_asteroid, and total_number_of_asteroids class methods

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -8,6 +8,14 @@ Figaro.load
 
 class NearEarthObjects
 
+  attr_reader :name, :diameter, :miss_distance
+
+  def initialize(data)
+    @name = data[:name]
+    @diameter = data[:estimated_diameter][:feet][:estimated_diameter_max].to_i
+    @miss_distance = data[:close_approach_data][0][:miss_distance][:miles].to_i
+  end
+
   def self.parse_asteroids_data(date)
     conn = Faraday.new(
       url: 'https://api.nasa.gov',
@@ -16,25 +24,5 @@ class NearEarthObjects
     asteroids_list_data = conn.get('/neo/rest/v1/feed')
 
     JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
-  end
-
-  def self.asteroid_list(date)
-    parse_asteroids_data(date).map do |asteroid|
-      {
-        name: asteroid[:name],
-        diameter: "#{asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
-        miss_distance: "#{asteroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
-      }
-    end
-  end
-
-  def self.biggest_asteroid(date)
-    parse_asteroids_data(date).map do |astroid|
-      astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
-    end.max { |a,b| a <=> b}
-  end
-
-  def self.total_number_of_asteroids(date)
-    parse_asteroids_data(date).count
   end
 end
